d6ba93e054d75a1819839d57681c12aef6f59684
Brian Wagner <brianthetall@gmail.com>
2020-09-19 17:21:53 -0400

    small addition to test passing by reference in the lambda.

--- lambda.cpp
+++ lambda.cpp
@@ -8,8 +8,11 @@ int main(){
 
   vector<double> vd{1.0,3.3,88.3,66.};
   for_each(vd.begin(),vd.end(),
-	   [](double& d){ cout << d << endl; }
+	   [](double& d){ cout << d++ << endl; }
 	   );
+
+  for(auto d : vd)
+    cout << d << endl;
 	     
 
 
