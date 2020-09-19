15ee4851e117ecba43682a0ec67c6dfe728fc8d1
Brian Wagner <brianthetall@gmail.com>
2020-09-19 16:48:38 -0400

    added a simple lambda function

---
+++ lambda.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+
+  vector<double> vd{1.0,3.3,88.3,66.};
+  for_each(vd.begin(),vd.end(),
+	   [](double& d){ cout << d << endl; }
+	   );
+	     
+
+
+  return 0;
+}
