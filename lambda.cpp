b2a19fb60233d4a3b746f19f778d47c4ddb5c692
Brian Wagner <brianthetall@gmail.com>
2020-09-20 01:13:53 -0400

    Changed to auto in lambda function.

--- lambda.cpp
+++ lambda.cpp
@@ -8,7 +8,7 @@ int main(){
 
   vector<double> vd{1.0,3.3,88.3,66.};
   for_each(vd.begin(),vd.end(),
-	   [](double& d){ cout << d++ << endl; }
+	   [](auto& d){ cout << d++ << endl; }
 	   );
 
   for(auto d : vd)
