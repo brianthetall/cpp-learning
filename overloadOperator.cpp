c56593a275018b06f5eaaeb867ff6f65e9cd2b2c
Brian Wagner <brianthetall@gmail.com>
2020-09-26 02:00:19 -0400

    example overloading the stream operator. Use this instead of a toString function.

---
+++ overloadOperator.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+class tea{
+  string teaType;
+  
+public:
+
+  tea(string name):teaType{name}{}
+  ~tea(){}
+  string getType(){return teaType;}
+  
+};
+
+ostream& operator<<(ostream& stream,tea& t){
+  stream << t.getType();
+  return stream;
+}
+
+int main(void){
+
+  tea gt{"green"};
+  tea bt{"black"};
+
+  cout << gt << endl;
+  cout << bt << endl;
+
+  return 0;
+}
