a6c5ee253f4c4782c7147167ee3ecb83a33b7e7c
Brian Wagner <brianthetall@gmail.com>
2020-09-26 03:27:57 -0400

    Left a hint about setting up apache

--- cgi/hello.cpp
+++ cgi/hello.cpp
@@ -5,7 +5,9 @@ using namespace std;
 int main(void){
 
   cout<<"<html><head><title>C++ from CGI</title></head>"<<endl;
-  cout<<"<body><h1>Hello from C++</h1></body></html>"<<endl;
+  cout<<"<body><h1>Hello from C++</h1>"<<endl;
+  cout<<"LoadModule cgid_module libexec/apache24/mod_cgid.so"<<endl;
+  cout<<"</body></html>"<<endl;
   
   return 0;
 }
