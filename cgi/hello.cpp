eae8b3157aa2d9bdbb55a2ef79a8acf9b4b589a9
Brian Wagner <brianthetall@gmail.com>
2020-09-26 02:32:50 -0400

    Trying C++ with CGI & Apache

---
+++ cgi/hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+
+  cout<<"<html><head><title>C++ from CGI</title></head>"<<endl;
+  cout<<"<body><h1>Hello from C++</h1></body></html>"<<endl;
+  
+  return 0;
+}
