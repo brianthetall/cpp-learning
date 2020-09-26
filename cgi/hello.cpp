8a0d04f06bf294026205d1bf58777f5a9ca33032
Brian Wagner <brianthetall@gmail.com>
2020-09-26 08:35:44 +0000

    Tweak to CGI

--- cgi/hello.cpp
+++ cgi/hello.cpp
@@ -4,9 +4,10 @@ using namespace std;
 
 int main(void){
 
+  cout << "Content-type:text/html\r\n\r\n";
   cout<<"<html><head><title>C++ from CGI</title></head>"<<endl;
-  cout<<"<body><h1>Hello from C++</h1>"<<endl;
-  cout<<"LoadModule cgid_module libexec/apache24/mod_cgid.so"<<endl;
+  cout<<"<body><h1>Hello from C++</h1>";
+  cout<<"<br>LoadModule cgid_module libexec/apache24/mod_cgid.so"<<endl;
   cout<<"</body></html>"<<endl;
   
   return 0;
